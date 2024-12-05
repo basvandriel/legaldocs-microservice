bcb6174f0bc2d6dc58a53fa42928142e1f22b80f
Bas van Driel <contact@basvandriel.nl>
2024-12-05 18:42:52 +0100

    test w

--- upload_to_ghpages.sh
+++ upload_to_ghpages.sh
@@ -1,7 +1,7 @@
 # GHPAGE_REPO_SSHLINK="git@github.com:basvandriel/legal-pdf-microservice-gh-pages.git"
 
 
-GHPAGE_REPO_SSHLINK="https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/basvandriel/legal-pdf-microservice-gh-pages"
+GHPAGE_REPO_SSHLINK="https://x-access-token:$GITHUB_TOKEN@github.com/basvandriel/legal-pdf-microservice-gh-pages"
 
 # Save the git repo path
 GIT_REPO_DIRECTORY=$(dirname "$0")
