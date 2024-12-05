0f22d96b8213add10d4496547e931305200cfdd2
Bas van Driel <contact@basvandriel.nl>
2024-12-05 18:06:57 +0100

    Push to repo

--- upload_to_ghpages.sh
+++ upload_to_ghpages.sh
@@ -5,25 +5,31 @@ GIT_REPO_DIRECTORY=$(dirname "$0")
 
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
 
-# Get the path to the repository
-GIT_REPO_PATH="$(dirname "$SCRIPT")/data/ghpages"
+TEMP_REPO_PATH="$(dirname "$SCRIPT")/data/tmp/legal-pdf-microservice-gh-pages"
+
+mkdir "$(dirname "$SCRIPT")/data/tmp/"
+mkdir $TEMP_REPO_PATH
+
+# Copy all the ghpages content
+cp -fr "$SCRIPT_PATH/data/ghpages/" "$TEMP_REPO_PATH"
 
 # Upload the English terms
-python -m app.cli data/TERMS_EN.md $GIT_REPO_PATH/terms.pdf
-
-# Remove the old repository
-rm -fr $GIT_REPO_PATH/.git
+python -m app.cli data/TERMS_EN.md $TEMP_REPO_PATH/terms.pdf
 
 # Initialize the temporary repository
-git init $GIT_REPO_PATH
+git init $TEMP_REPO_PATH
 
-# Add the remote
-git -C $GIT_REPO_PATH remote add origin $GHPAGE_REPO_SSHLINK
+# # Add the remote
+git -C $TEMP_REPO_PATH remote add origin $GHPAGE_REPO_SSHLINK
 
-# Stage all the files and commit
-git -C $GIT_REPO_PATH add .
-git -C $GIT_REPO_PATH commit -m "Update legal terms"
+# # Stage all the files and commit
+git -C $TEMP_REPO_PATH add .
+git -C $TEMP_REPO_PATH commit -m "Update legal terms"
 
-# Force push, ensuring we only have one commit
-git -C $GIT_REPO_PATH push --set-upstream origin main --force
+# # Force push, ensuring we only have one commit
+git -C $TEMP_REPO_PATH push --set-upstream origin main --force
+
+# Clean up
+rm -fr $TEMP_REPO_PATH
